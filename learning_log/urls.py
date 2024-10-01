be5a8c8e5af23cd724f0d7088d738cbc897de495
mostafa-faisal <salfaymf8@gmail.com>
2024-10-01 17:29:23 +0600

    fav

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include("users.urls")),
     path('',include("learning_logs.urls")),
+    path('favicon.ico', RedirectView.as_view(url='static/images/favicon.ico')),
     
 ]
+
+
+
