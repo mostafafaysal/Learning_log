c74ad09f0744b433be08595680f71453ab984bdc
mostafa-faisal <salfaymf8@gmail.com>
2024-10-01 17:13:40 +0600

    debug false

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@1^y8yxdex5d4b-+393gt92$cm05jx2$1wy8^d2nmq5k%190ku'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
